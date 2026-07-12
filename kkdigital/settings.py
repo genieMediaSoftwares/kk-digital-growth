053a85d593e37014d2082086a39427a657d124c8
genieMediaSoftwares <kkdigitalgrowthsoftwares@gmail.com>
2026-07-12 12:29:56 +0530

    Updated mobile responsive design

--- kkdigital/settings.py
+++ kkdigital/settings.py
@@ -29,9 +29,9 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 ALLOWED_HOSTS = [
-    ".vercel.app",
-    "localhost",
-    "127.0.0.1",
+    "kkdigitalgrowth.com",
+    "www.kkdigitalgrowth.com",
+
 ]
 CSRF_TRUSTED_ORIGINS = [
     "https://*.vercel.app",
