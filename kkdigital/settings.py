dfda2388a9c5153c7fc19078b07b7872b4ceed56
genieMediaSoftwares <kkdigitalgrowthsoftwares@gmail.com>
2026-07-12 12:48:47 +0530

    Updated mobile responsive design

--- kkdigital/settings.py
+++ kkdigital/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = [
     "127.0.0.1",
     ".vercel.app",
     "kk-digital-growth-66hrl8bw7-kkdigitalsoftwaregrowth-s-projects.vercel.app",
+    "kkdigitalgrowth.com",
+    "www.kkdigitalgrowth.com"
 ]
 
 CSRF_TRUSTED_ORIGINS = [
